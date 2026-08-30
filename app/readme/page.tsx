479981015ab3fef84c44f213314e82273eac8747
AsfarButt <asfarbutt69@gmail.com>
2026-08-30 12:45:33 -0700

    working on llm summary

--- app/readme/page.tsx
+++ app/readme/page.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar"
 export default function Page(){
     return(<div className="relative w-full h-full flex flex-row">
         <Sidebar />
-        <div className="relative h-full w-fill bg-black">
+        <div className="relative h-full w-fill bg-black text-white font-light">
         <h1>YO, Wassip</h1>
         <h3>README Page will be displayed here</h3>
         </div>
--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,11 +1,13 @@
-import Sidebar from "../../components/sidebar"
+import LlmSummary from "./llmsummary";
 
-export default function Page(){
-    return(<div className="relative w-full h-full flex flex-row">
-        <Sidebar />
-        <div className="relative h-full w-fill bg-black">
-        <h1>YO, Wassip</h1>
-        <h3>LLM Page will be displayed here</h3>
-        </div>
-    </div>)
+export const dynamic = 'force-dynamic';
+
+export default async function Page() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="w-full h-full overflow-scroll">
+        <LlmSummary />
+      </main>
+    </div>
+  );
 }
